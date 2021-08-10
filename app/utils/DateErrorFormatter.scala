d0ed1b7780cef90bea418c2c917def344e3739a5
carrielies <carrielies@users.noreply.github.com>
2021-08-10 15:19:08 +0100

    TRUS-4291 Fix Error Date Display

--- app/utils/DateErrorFormatter.scala
+++ app/utils/DateErrorFormatter.scala
@@ -22,7 +22,8 @@ import play.api.i18n.Messages
 object DateErrorFormatter {
 
   def formatArgs(args: Seq[Any])(implicit messages: Messages): Seq[String] = {
-    args.map(arg => messages(s"date.$arg").toLowerCase)
+    val dateArgs = Seq("day", "month", "year")
+    args.map(arg => if (dateArgs.contains(arg)) messages(s"date.$arg").toLowerCase else arg.toString)
   }
 
   def addErrorClass(error: Option[FormError], dateArg: String): String = {
