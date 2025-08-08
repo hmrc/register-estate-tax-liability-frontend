a98bbaf1308fa9f5abe5b029df4b9759827b28ea
amruta-lohote <203861386+amruta-lohote@users.noreply.github.com>
2025-08-08 16:25:37 +0100

    DDCE-6958- Updated dependencies,  removed Scalastyle and used ServicesConfig

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,8 +1,8 @@
 import sbt.*
 
 object AppDependencies {
-  val bootstrapVersion = "9.11.0"
-  val mongoVersion = "2.6.0"
+  val bootstrapVersion = "9.19.0"
+  val mongoVersion = "2.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                     % mongoVersion,
@@ -16,7 +16,7 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-30"   % bootstrapVersion,
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-30"  % mongoVersion,
-    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.18.0",
+    "org.scalatestplus"           %% "scalacheck-1-18"          % "3.2.19.0",
     "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2"
   ).map(_ % Test)
 
